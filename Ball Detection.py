5a6b9640872afd8ec44c782569bb5a2e673b0b35
Rohan050198 <rohdes1876@gmail.com>
2023-01-04 15:15:40 +0530

    initial commit

---
+++ Ball Detection.py
@@ -0,0 +1,38 @@
+import cv2 as cv
+import numpy as np
+
+videoCapture=cv.VideoCapture(0)
+prevCircle=None
+dist= lambda x1,y1,x2,y2: (x1-x2)^2+(y1-y2)^2
+while(videoCapture.isOpened()):
+    ret, frame=videoCapture.read()
+if not ret:
+    break
+
+grayFrame=cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+blurFrame=cv.GaussianBlur(grayFrame,(21,21),0)
+
+circles=cv.HoughCircles(blurFrame,cv.HOUGH_GRADIENT,1.2,40,param1=100,param2=30,minRadius=75,maxRadius=500)
+if circles is not None:
+    circles=np.uint16(np.around (circles))
+    chosen=None
+    for i in circles[0,:]:
+        if chosen is None: chosen=i
+        if prevCircle is not None:
+            if dist(chosen[0],chosen[1],prevCircle[0],prevCircle[1]) <= dist(i[0],i[1],prevCircle[0],prevCircle[1]):
+                chosen=i
+       cv.circle(frame,(chosen[0],chosen[1]),1,(0,100,100),3)
+    cv.circle(frame,(chosen[0],chosen[1],chosen[2],(255,0,255),3))
+    prevCircle=chosen
+
+    cv.imshow("circles",frame)
+
+
+
+cv.imshow('blurFrame',blurFrame)
+#cv.imshow("FRAME",frame)
+if cv.Waitkey(1) & 0xFF == ord('q'):
+         break
+
+videoCapture.release()
+cv.destroyAllWindows()
---
+++ README.md
@@ -0,0 +1,13 @@
+
+# Coloured ball detector
+
+Using supervised learning, the following project is able to distinguish between spheres of different colour based on the colour parameters specified by the user.
+
+
+
+## Demo
+
+https://drive.google.com/file/d/1g8So6XI9v9nEYhSPMwN1zeo24qzdaE6S/view?usp=sharing
+
+![Logo](https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTynRRT0IEiIbTC0bbRvtcOg6b4Ah_jwzwRxQ&usqp=CAU)
+
